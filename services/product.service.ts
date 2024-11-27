071e952f18aeff7c2015d92a9264622ea94a996b
Ilnur Gareev <GareevIIld@tatneft.tatar>
2024-11-27 11:37:51 +0300

    fix

--- services/product.service.ts
+++ services/product.service.ts
@@ -1,7 +1,7 @@
 import { IProduct } from '@/interfaces/product.interface';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'http://127.0.0.1:3000';
 
 axios.defaults.baseURL = API_URL;
 
